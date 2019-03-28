fbfde37cd3d0a18a565634841dbce6428766498e
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-03-28 10:20:52 +0100

    zapisuje planszę do pliku txt

---
+++ zapisDoTxt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "Plansza_t.h"
+void doPlikuTxt(char * nazwa, plansza_t plansza){
+    FILE * plik=fopen(nazwa, "w");
+    int w=plansza.wiersze;
+    int k=plansza.kolumny;
+    for(int i=0;i<w; i++){
+        for(int j=0;j<k;j++){
+            fprintf(plik,"%d ",plansza.tablica[i*k+j]);
+            }
+            fprintf(plik,"\n");
+
+    }
+    fclose (plik);
+    }
+

c41e69d8c4ede6bada6bd039114b27005bd0e958
KasiaWiktoria <48280406+KasiaWiktoria@users.noreply.github.com>
2019-04-03 09:41:24 +0200

    poprawienie obsługi flag

--- nagłówki/Plansza_t.h
+++ nagłówki/Plansza_t.h
@@ -1,8 +1,11 @@
-#pragma once
+#ifndef PLANSZA_T
+#define PLANSZA_T
+
 typedef struct plansza_t {
 	int * tablica;
 	int wiersze;
 	int kolumny;
 }plansza_t;
 
-void wyslwietlPansze(plansza_t plansza);
+void wyslwietlPlansze(plansza_t plansza);
+#endif

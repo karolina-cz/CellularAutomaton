e9e729c99c83bcd22fd21dd0d8191660d587d3bd
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-04-06 19:45:14 +0200

    Add files via upload

---
+++ testy/testGeneratora/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"plansza_t.h"
+#include "flagi_t.h"
+void przeprowadzGreWZycie(plansza_t, flagi_t, void(*doTxt)(plansza_t,char*),void(*doPng)(plansza_t*, char*));
+int main()
+{
+    plansza_t plansza;
+    plansza.kolumny=4;
+    plansza.wiersze=4;
+    int b[16]={1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1};
+    plansza.tablica=malloc(sizeof(int)*16);
+    for(int i=0;i<16;i++)
+        plansza.tablica[i]=b[i];
+    flagi_t flagi;
+    flagi.ileGeneracji=2;
+        flagi.wyjsciowyT= "test";
+        flagi.wyjsciowyG= "grafika";
+        flagi.formatZapisu=0; //zapis do txt i tak nie nastapi bo to test tylko generatora
+        przeprowadzGreWZycie(plansza,flagi,NULL,NULL);
+        free(plansza.tablica);
+    return 0;
+}

4f74ca0da1803c936c732d24ea3634a5feb8a50b
KasiaWiktoria <48280406+KasiaWiktoria@users.noreply.github.com>
2019-04-06 20:55:28 +0200

    naprawienie funkcji wczytującej

--- funkcje/inicjacjaPlanszyZPliku.c
+++ funkcje/inicjacjaPlanszyZPliku.c
@@ -1,77 +1,88 @@
 #include "inicjacjaPlanszyZPliku.h"
 #pragma warning(disable : 4996)
 
-plansza_t wczytajZPliku(const char* plik)
-{
+plansza_t wczytajZPliku(const char* plik){
+
     FILE* in = fopen(plik, "r");
 
     plansza_t a;
 
-    policz(in, &a);
+    policz(plik, &a);
     a.tablica = malloc(sizeof(int) * a.wiersze * a.kolumny);
-    int k = 0, w = 0, k2 = 0, n = 0;
-    if( sprawdz(in, &a) == -1 )
+
+    if( sprawdz(plik, &a) == -1 )
         return inicjacjaLosowa(10, 10);
-    else if (sprawdz(in, &a) == 0)
-    {
-        int n = 0;
-        while (getc(in) != EOF)
-        {
-            a.tablica[n++] = getc(in);
-            return a;
-        }
-    }
+
+	else if (sprawdz(plik, &a) == 0) {
+
+		/*for (int i = 0; i < a.wiersze*a.kolumny; i++) {
+			a.tablica[i] = getc(in) - '0';
+		}*/
+		wypelnij(plik, &a);
+	}
+	fclose(in);
+	return a;
 }
 
-plansza_t wypelnij( FILE* in, plansza_t *x )
-{
+void wypelnij(const char* plik, plansza_t* x ){
+
+	FILE* in = fopen(plik, "r");
 
     int n = 0;
-    while (getc(in) != EOF)
-    {
-        x->tablica[n++] = getc(in);
+	char znak;
+    while ((znak = getc(in)) != EOF){
+		if (znak != '\n') {
+			x->tablica[n++] = znak - '0';
+		}
     }
-    return *x;
+	fclose(in);
 }
 
-void policz( FILE* in, plansza_t* x)
-{
+void policz(const char* plik, plansza_t* x){
 
-    int k=0,w=0;
-    char c=getc(in);
-    while ( c != EOF && c!= '\n')
-    {
+	FILE* in = fopen(plik, "r");
+    int k=0, w;
+    char znak;
+    while ( (znak = getc(in)) != EOF && (znak != '\n'))
         k++;
-        c=getc(in);
-    }
+    
+	printf("Liczba kolumn: %d\n", k);
     x->kolumny=k;
+
     w=1; //bo przeczytano juz 1 linie
-    c=getc(in);
-    while ( c != EOF )
-    {
-        if(c =='\n')
+    while ( (znak=getc(in)) != EOF )
+        if(znak == '\n')
             w++;
-        c=getc(in);
-    }
+    
     w++; // bo na koncu nie bylo znaku konca linnii tyko EOF
+	printf("Liczba wierszy: %d \n", w); 
     x->wiersze=w;
 
+	fclose(in);
 }
 
-int sprawdz(FILE* in, plansza_t* x)
-{
-
-    int k = 0, w = 0, k2 = 0;
-    while ( getc(in) != EOF)
+int sprawdz(const char* plik, plansza_t* x)
+{	
+	/*
+	FILE* in = fopen(plik, "r");
+    int k = x->kolumny, w = 0, k2;
+	char d;
+    while ( (d=getc(in)) != EOF)
     {
-        while (getc(in) != '\n')
+		k2 = 0;
+        while ( (d= getc(in) != '\n') && (getc(in) != EOF))
         {
             k2++;
         }
-        if (k2 != k)
-            printf("Liczba znakow w wierszu %d jest nieprawidlowa, wiersz powinien zawierac %d znakow, a znaleziono %d \n", w, k, k2);
-        return -1;
+		if (k2 != k) {
+			//
+			//printf("Liczba wierszy: %d \n", w);
+			printf("Liczba znakow w wierszu %d jest nieprawidlowa, wiersz powinien zawieraznak%d znakow, a znaleziono %d \n", w, k, k2);
+			return -1;
+		}
         w++;
     }
+	fclose(in);
+	*/
     return 0;
 }

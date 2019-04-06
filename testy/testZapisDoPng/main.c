37f35e556231c33c920079ab2127e36a0fbb3aec
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-04-06 15:29:23 +0200

    test zapisDoPng

---
+++ testy/testZapisDoPng/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+#include "Plansza_t.h"
+void doPng(plansza_t *, char*);
+int main()
+{   //tworzenie losowej planszy
+    srand(time(NULL));
+    plansza_t plansza;
+    plansza.wiersze=10;
+    plansza.kolumny=10;
+    plansza.tablica=malloc(sizeof(int)*plansza.wiersze*plansza.kolumny);
+    for(int i=0;i<plansza.wiersze*plansza.kolumny;i++)
+        plansza.tablica[i]=rand()%2;
+    //weswietlam plansze, zeby mozna bylo porownac z plikiem graficznym
+    for(int i=0; i<plansza.wiersze; i++)
+    {
+        for(int j=0; j<plansza.kolumny; j++)
+        {
+            printf("%d ",plansza.tablica[i*plansza.kolumny+j]);
+        }
+        printf("\n");
+    }
+    doPng(&plansza,"test_grafiki.png");
+
+
+        return 0;
+}

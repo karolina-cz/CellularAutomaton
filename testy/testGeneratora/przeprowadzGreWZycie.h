6e5f067f115a02cfc8a29ecae46bfbc6d9700d7b
karolina-cz <48332322+karolina-cz@users.noreply.github.com>
2019-04-08 21:44:08 +0200

    Create przeprowadzGreWZycie.h

---
+++ testy/testGeneratora/przeprowadzGreWZycie.h
@@ -0,0 +1,15 @@
+    
+#ifndef przeprowadz_Gre_W_Zycie
+#define przeprowadz_Gre_W_Zycie
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "plansza_t.h"
+#include "flagi_t.h"
+#include<string.h>
+
+void doPng(plansza_t *, char* );
+void doTxt( plansza_t , char *);
+void przeprowadzGreWZycie(plansza_t , flagi_t , void(*doTxt)(plansza_t,char*),void(*doPng)(plansza_t*, char*));
+
+#endif
